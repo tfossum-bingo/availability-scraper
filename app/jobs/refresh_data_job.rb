49d2c9c4c1de17e0e4a80ccd5e37ff5400faa262
Ted Fossum <theodorefossum@gmail.com>
2021-02-20 20:19:27 -0500

    Control refresh frequency based on environment.

--- app/jobs/refresh_data_job.rb
+++ app/jobs/refresh_data_job.rb
@@ -2,7 +2,13 @@ class RefreshDataJob < ApplicationJob
   queue_as :default
 
   after_perform do |job|
-    self.class.set(:wait => 10.minutes).perform_later
+    delay_minutes =
+    if Rails.env.production?
+      240
+    else
+      10
+    end
+    self.class.set(:wait => delay_minutes.minutes).perform_later
   end
 
   def perform(*args)
